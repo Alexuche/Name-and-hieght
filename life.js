130463e548bdd5dab6ef454117bcbcf0e75001b0
Alex Uche <nnayidb@outlook.com>
2022-06-23 20:43:56 +0100

    first commit

---
+++ life.js
@@ -0,0 +1,15 @@
+//var birth = prompt("Please input your Birth year")
+//var now = new Date()
+//var age = now.getYear() - birth + 1900
+
+//if(age<18)
+   // console.log('minor')
+//else if(age>=18 && age<36)
+   // console.log('youth')
+//else console.log('elder')
+
+
+
+var person = {Name: "Nnayi", Height: "5", Country: "Nigerian"};
+document.getElementById("demo").innerHTML =
+person.Name + " is " + person.Height + "ft tall" + " and he is a "  + person.Country ;
